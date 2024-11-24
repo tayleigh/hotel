88e981f929149749c49a010e42543023d70de73c
Tayleigh Ward <tayleigh.ward24@gmail.com>
2024-11-24 01:21:17 -0700

    not awful?????

---
+++ data/hotels.js
@@ -0,0 +1,50 @@
+export const hotels = [
+    {
+        "name": "Arches National Park",
+        "address": ["Arches National Park","Moab, UT 84532, USA"],
+        "phone": "555-123-4567",
+        "image": "arches.webp"
+      },
+      {
+        "name": "Glacier National Park",
+        "address": ["Glacier National Park","West Glacier, MT 59936, USA"],
+        "phone": "555-234-5678",
+        "image": "glacier.webp"
+      },
+      {
+        "name": "Yellowstone National Park",
+        "address": ["Yellowstone National Park","Yellowstone, WY 82190, USA"],
+        "phone": "555-345-6789",
+        "image": "yellostone.webp"
+      },
+      {
+        "name": "Grand Teton National Park",
+        "address": ["Grand Teton National Park","Moose, WY 83012, USA"],
+        "phone": "555-456-7890",
+        "image": "teton.webp"
+      },
+      {
+        "name": "Capitol Reef National Park",
+        "address": ["Capitol Reef National Park","Torrey, UT 84775, USA"],
+        "phone": "555-567-8901",
+        "image": "capital-reef.webp"
+      },
+      {
+        "name": "Grand Canyon National Park",
+        "address": ["Grand Canyon National Park","Grand Canyon, AZ 86023, USA"],
+        "phone": "555-678-9012",
+        "image": "grandcanyon.webp"
+      },
+      {
+        "name": "Zion National Park",
+        "address": ["Zion National Park","Springdale, UT 84767, USA"],
+        "phone": "555-789-0123",
+        "image": "zion.webp"
+      },
+      {
+        "name": "Yosemite National Park",
+        "address": ["Yosemite National Park","Yosemite Valley, CA 95389, USA"],
+        "phone": "555-890-1234",
+        "image": "yosemite.webp"
+      }
+]
